29358093b728a9802c4722d567df0358fb7b8b74
Kphillycat <keithwilliamsjr@gmail.com>
2013-11-15 09:11:16 -0500

    Add todo4/

---
+++ todo4/count_sentences.rb
@@ -0,0 +1,10 @@
+# Write a method on String called `count_sentences` that returns the number of
+# sentences in the string it is called on
+
+class String
+  def count_sentences
+    counter = 0
+    self.split(".").each {|x| counter+=1}
+	counter
+  end
+end
