f0f7712eb7b331b3177974be3bdb2bf8846c094b
Kphillycat <keithwilliamsjr@gmail.com>
2013-11-18 18:43:54 -0500

    Add more it...expect...do blocks to yahtzee_spec.rb

--- dog_years_and_yahtzee/yahtzee/yahtzee_spec.rb
+++ dog_years_and_yahtzee/yahtzee/yahtzee_spec.rb
@@ -1,8 +1,21 @@
-require_relative 'yahtzee.rb'
+require_relative 'yahtzee'
 
 describe "#roll_dice" do
+	#roll_dice_Array = roll_dice
+
 	it "should return a new array" do
 		expect(roll_dice).to be_a(Array)
+	end
+	
+	it "should return a new array of length 5" do
+		expect(roll_dice.length).to eq(5)
+	end
+
+	it "should return a number between 1 and 6" do
+		roll_dice.each do |die|
+			expect(die).to be > 0
+			expect(die).to be < 7
+		end
 	end
 end
 
