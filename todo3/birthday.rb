2ba38d1680635c583ef3c77b0f72b55c293f3243
Kphillycat <keithwilliamsjr@gmail.com>
2013-11-19 15:50:21 -0500

    Add birthday.rb

--- todo3/birthday.rb
+++ todo3/birthday.rb
@@ -1,3 +1,11 @@
-puts "What's your birthday (mm/dd/yy)?"
+puts "What's your birthday (mm/dd/yyyy)?"
 bday = gets.chomp
-puts Time.new.day
+day = Time.new.to_a[3]
+month = Time.new.to_a[4]
+year = Time.new.to_a[5]
+bday_arr = bday.split("/")
+if month == bday_arr[0].to_i && day == bday_arr[1].to_i && year == bday_arr[2].to_i
+	puts "Happy Birthday!"
+else
+	puts "....mmmmk"
+end
