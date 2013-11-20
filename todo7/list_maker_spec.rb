7e8a3c9c25f30e675fdda06de8347e924d187747
Kphillycat <keithwilliamsjr@gmail.com>
2013-11-20 09:44:41 -0500

    Refactored iterator in test to check specific values in array

--- todo7/list_maker_spec.rb
+++ todo7/list_maker_spec.rb
@@ -7,15 +7,10 @@ describe "#make_list" do
 
 	it "should return an Array of strings with (index+1)." do
 		index = 0
-		make_list(["ich", "ni", "san"]).each do |word|
-			expect(word).to be((index+1).to_s + ". " + word)
+		word_array = make_list(["ich", "ni", "san"])
+		["ich", "ni", "san"].each do |word|
+			expect(word_array[index]).to eq((index+1).to_s + ". " + word)
 			index+=1
 		end
 	end
-	
-	# roll_dice.each do |die|
-	# 		expect(die).to be > 0
-	# 		expect(die).to be < 7
-	# 	end
-
 end
