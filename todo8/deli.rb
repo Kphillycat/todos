3736d8018f41d6c478747004a61ac330f6f6a429
Kphillycat <keithwilliamsjr@gmail.com>
2013-11-21 10:17:29 -0500

    Add code to yield passing tests

--- todo8/deli.rb
+++ todo8/deli.rb
@@ -6,11 +6,18 @@ class Deli
 	end
 
 	def take_a_number(name)
-		@line.each_with_index do |value, index|
-			@line[index] = "#{index}. #{name}"
-		end 
+		#@line.length is 3 for some reason
+		@line << "#{@line.length+1}. #{name}"
+		# @line.each_with_index do |value, index|
+		# 	@line[index] = "#{index+1}. #{name}"
+		# end 
 	end
 
 	def now_serving
+		num_regex = /\d+. /
+		name_n_num = @line.delete_at(0)
+		name = name_n_num.sub num_regex, ''
+		name
 	end
-end
+
+end #end of class
