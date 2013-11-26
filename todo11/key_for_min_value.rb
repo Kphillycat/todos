8106ad2a43e1bc51e2c29c4b49b200b10e83a10f
Kphillycat <keithwilliamsjr@gmail.com>
2013-11-26 09:26:49 -0500

    remove given tests and replaced with own test specific to format of solution method. Add value for theorectical MAX number

--- todo11/key_for_min_value.rb
+++ todo11/key_for_min_value.rb
@@ -3,7 +3,8 @@
 def key_for_min_value(hash)
   return nil if hash.empty?
   min_value_key = nil
-  min_value = 100000
+  min_value = (2**(0.size * 8 -2) -1) #Theorectical Fixnum_max 
+  #http://stackoverflow.com/questions/535721/ruby-max-integer
   hash.each do |key, value|
   	if value < min_value
   		min_value_key = key 
--- todo11/spec.rb
+++ todo11/spec.rb
@@ -1,16 +1,22 @@
 require_relative "./key_for_min_value"
 
-describe "smallest hash value" do
-  it "should return the key of the smallest hash value" do
-    key_for_min_value({:blake => 500, :ashley => 2, :adam => 1}).should eq(:adam)
-  end
+describe "#key_for_min_value" do
+	it "should return the key of the min value of hash" do
+		expect(key_for_min_value({:test_1 => 100, :test_2 => 20, :test_3 => 2, :test_4 => 400})).to eq(:test_3)
+	end
 
-  it "should return the key of the smallest hash value example 2" do
-    key_for_min_value({:blake => 10, :ashley => 50, :adam => 17}).should eq(:blake)
-  end
+	it "should return nil for empty hash" do
+		expect(key_for_min_value({})).to eq(nil)
+	end
 
-  it "should return nil for an empty hash" do
-    key_for_min_value({}).should eq(nil)
-  end
+	it "should return min value even if 100000 is in hash" do
+		expect(key_for_min_value({:test_1 => 100000, :test_2 => 20, :test_3 => 2, :test_4 => 400})).to eq(:test_3)
+	end
 
+	it "hould return min value even if FIXNUM_MAX is in hash" do
+		FIXNUM_MAX = (2**(0.size * 8 -2) -1)
+		#http://stackoverflow.com/questions/535721/ruby-max-integer
+		expect(key_for_min_value({:test_1 => 100000, :test_2 => FIXNUM_MAX, :test_3 => 200000, :test_4 => 400000})).to eq(:test_1)
+
+	end
 end
