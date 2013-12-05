ba48c4a55ef2bfb8efa7efa2899bc7daba467034
Kphillycat <keithwilliamsjr@gmail.com>
2013-12-05 09:31:14 -0500

    Add code to raise errors

--- todo14/triangle.rb
+++ todo14/triangle.rb
@@ -1,13 +1,33 @@
+class TriangleError < StandardError
+end
+
 class Triangle
-	attr_accessor :side_a, :side_b, :side_c
+	require 'debugger'
+
+	attr_accessor :side_a, :side_b, :side_c, :sides
 
 	def initialize(side_a, side_b,side_c)
 		@side_a = side_a
 		@side_b = side_b
 		@side_c = side_c
+		@sides = [side_a, side_b, side_c]
 	end
 
 	def kind
+		sum = side_a + side_b
+
+		if sides.any? {|side| side < 0}
+			raise TriangleError
+		end
+
+		if sum < side_c
+			raise TriangleError
+		end
+
+		if sides.inject{|sum,x| sum + x } == 0
+			raise TriangleError
+		end
+
 		if side_a == side_b && side_b == side_c
 			return :equilateral
 		end
@@ -21,5 +41,7 @@ class Triangle
 		end
 
 
+
+		
 	end
 end
