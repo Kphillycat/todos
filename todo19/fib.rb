5077f7eeea18c6abef2700d0158756b9f596ee12
Kphillycat <keithwilliamsjr@gmail.com>
2013-12-13 10:03:23 -0500

    remove unnessary code

--- todo19/fib.rb
+++ todo19/fib.rb
@@ -3,11 +3,9 @@
 # fib sequence = 0,1,1,2,3,5,8,13 etc, assume the 1st element is 0 (the first number), the 2nd element is 1, the third element is 1
 
 def fibo_finder(n)
- fib = [0,1]
- (n+1).times do |i|
- 	temp = fib
- 	fib << fib[i-1] + fib[i-2] unless i == 0 || i == 1
- 	
- end
-fib[n]
+	fib = [0,1]
+	(n+1).times do |i| 	
+ 		fib << fib[i-1] + fib[i-2] unless i == 0 || i == 1 	
+	end
+	fib[n]
 end
