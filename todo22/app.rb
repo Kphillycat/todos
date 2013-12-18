1a5c12bbfe523c5486935354630318580a04e20f
Kphillycat <keithwilliamsjr@gmail.com>
2013-12-18 09:16:07 -0500

    Add initial code to display whether it's my birthday or not at root route

---
+++ todo22/app.rb
@@ -0,0 +1,31 @@
+require 'bundler'
+Bundler.require
+
+Dir.glob('./lib/*.rb') do |model|
+  require model
+end
+
+module Name
+  class App < Sinatra::Application
+
+    #configure
+    configure do
+      set :root, File.dirname(__FILE__)
+      set :public_folder, 'public'
+    end
+
+    #routes
+    get '/' do
+      @time = Time.new
+      erb :index
+    end
+
+    #helpers
+    helpers do
+      def partial(file_name)
+        erb file_name, :layout => false
+      end
+    end
+
+  end
+end
