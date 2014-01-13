9ead202f0444ac9b1b7c84b0a20cc059954ac1d7
Kphillycat <keithwilliamsjr@gmail.com>
2014-01-13 09:33:38 -0500

    Add bare minimal scaffold with nested routes

---
+++ todo34/nested_resources/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NestedResources::Application.config.secret_key_base = '252cd92cadeb5b4cc185d000e6c94c1215c2f6f12e7468f192e8d141843720819d4b90d3049070ab1fa79e7b0361cd5f04cd01dafed3328f09ebb4a550f9e9a6'
---
+++ todo34/nested_resources/db/migrate/20140113135825_create_stories.rb
@@ -0,0 +1,9 @@
+class CreateStories < ActiveRecord::Migration
+  def change
+    create_table :stories do |t|
+      t.string :content
+			t.integer :song_id
+      t.timestamps
+    end
+  end
+end
